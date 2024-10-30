d876a4b1dfdefe8c45e7579165d86bb9d92c8176
Dhanarooban1 <dhanazx1@gmail.com>
2024-10-30 11:25:33 +0530

    Updated-theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Test from './Components/Test';
+
 import Homepage from './Components/Homepage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Test />} />
-        <Route path="/about" element={<Homepage />} />
+   
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </Router>
   );
